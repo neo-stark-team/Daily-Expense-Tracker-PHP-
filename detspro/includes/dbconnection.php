f629e3fe048e694c2e25b0575350e6925a212a1e
Mathankumar R <rmathankumarcpm@gmail.com>
2023-04-12 17:01:58 +0530

    First commit

---
+++ detspro/includes/dbconnection.php
@@ -0,0 +1,7 @@
+<?php
+$con=mysqli_connect("database-1.c7j0ofgsqkxi.us-east-1.rds.amazonaws.com", "admin", "examly2023", "detsdb");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+
+  ?>
